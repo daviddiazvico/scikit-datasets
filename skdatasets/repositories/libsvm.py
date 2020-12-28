f6aabe4130c93350da80a9ea32f5a0a8b17d7e30
vnmabus <vnmabus@gmail.com>
2020-12-28 18:09:24 +0100

    Libsvm repo now load existing datasets from disk.

--- skdatasets/repositories/libsvm.py
+++ skdatasets/repositories/libsvm.py
@@ -5,44 +5,61 @@ LIBSVM datasets (https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets).
 @license: MIT
 """
 
-import numpy as np
 import os
+from urllib.request import urlretrieve
+
+import numpy as np
 import scipy as sp
 from sklearn.datasets import (get_data_home, load_svmlight_file,
                               load_svmlight_files)
-from sklearn.utils import Bunch
 from sklearn.model_selection import PredefinedSplit
-from urllib.request import urlretrieve
+from sklearn.utils import Bunch
 
+from .base import fetch_file
 
 BASE_URL = 'https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets'
 COLLECTIONS = {'binary', 'multiclass', 'regression', 'string'}
 
 
-def _fetch_partition(collection, name, partition, dirname=None):
+def _fetch_partition(collection, name, partition, data_home=None):
     """Fetch dataset partition."""
-    filename = name.replace('/', '-') + partition
+    subfolder = os.path.join(get_data_home(data_home=data_home), 'libsvm',
+                             collection)
+    dataname = name.replace('/', '-')
+
     url = BASE_URL + '/' + collection + '/' + name + partition
     try:
-        f = filename + '.bz2' if dirname is None else os.path.join(dirname,
-                                                                   filename + '.bz2')
-        f, _ = urlretrieve(url + '.bz2', filename=f)
+        f = fetch_file(
+            dataname,
+            urlname=url + '.bz2',
+            subfolder=subfolder,
+            data_home=data_home,
+        )
     except:
         try:
-            f = filename if dirname is None else os.path.join(dirname, filename)
-            f, _ = urlretrieve(url, filename=f)
+            f = fetch_file(
+                dataname,
+                urlname=url,
+                subfolder=subfolder,
+                data_home=data_home,
+            )
         except:
             f = None
+
+    if f is not None:
+        f = os.fspath(f)
+
     return f
 
 
-def _load(collection, name, dirname=None):
+def _load(collection, name, data_home=None):
     """Load dataset."""
-    filename = _fetch_partition(collection, name, '', dirname=dirname)
-    filename_tr = _fetch_partition(collection, name, '.tr', dirname=dirname)
-    filename_val = _fetch_partition(collection, name, '.val', dirname=dirname)
-    filename_t = _fetch_partition(collection, name, '.t', dirname=dirname)
-    filename_r = _fetch_partition(collection, name, '.r', dirname=dirname)
+    filename = _fetch_partition(collection, name, '', data_home)
+    filename_tr = _fetch_partition(collection, name, '.tr', data_home)
+    filename_val = _fetch_partition(collection, name, '.val', data_home)
+    filename_t = _fetch_partition(collection, name, '.t', data_home)
+    filename_r = _fetch_partition(collection, name, '.r', data_home)
+
     if (filename_tr is not None) and (filename_val is not None) and (filename_t is not None):
         _, _, X_tr, y_tr, X_val, y_val, X_test, y_test = load_svmlight_files([filename,
                                                                               filename_tr,
@@ -98,12 +115,12 @@ def fetch(collection, name, data_home=None):
     """
     if collection not in COLLECTIONS:
         raise Exception('Avaliable collections are ' + str(list(COLLECTIONS)))
-    dirname = os.path.join(get_data_home(data_home=data_home), 'libsvm',
-                           collection, name.replace('/', '-'))
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
-    X, y, X_test, y_test, cv, X_remaining, y_remaining = _load(collection, name,
-                                                               dirname=dirname)
+
+    X, y, X_test, y_test, cv, X_remaining, y_remaining = _load(
+        collection,
+        name,
+        data_home=data_home,
+    )
     data = Bunch(data=X, target=y, data_test=X_test, target_test=y_test,
                  inner_cv=cv, outer_cv=None, data_remaining=X_remaining,
                  target_remaining=y_remaining, DESCR=name)
