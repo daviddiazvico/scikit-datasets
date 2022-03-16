0a6091885a6ba5b6b5c1f1031ed36df65b32cca6
vnmabus <vnmabus@gmail.com>
2022-03-16 11:41:53 +0100

    Change test paths.

--- skdatasets/tests/utils/test_run.py
+++ skdatasets/tests/utils/test_run.py
@@ -8,40 +8,40 @@ import subprocess
 
 def test_binary_classification():
     """Tests binary classification experiment."""
-    ret = subprocess.call(['tests/utils/run.py', '-r', 'keel', '-c',
+    ret = subprocess.call(['skdatasets/tests/utils/run.py', '-r', 'keel', '-c',
                            'imbalanced', '-d', 'abalone9-18', '-e',
-                           'tests/utils/MLPClassifier.json'])
+                           'skdatasets/tests/utils/MLPClassifier.json'])
     assert ret >= 0
-    ret = subprocess.call(['tests/utils/run.py', '-r', 'libsvm', '-c', 'binary',
+    ret = subprocess.call(['skdatasets/tests/utils/run.py', '-r', 'libsvm', '-c', 'binary',
                            '-d', 'breast-cancer', '-e',
-                           'tests/utils/MLPClassifier.json'])
+                           'skdatasets/tests/utils/MLPClassifier.json'])
     assert ret >= 0
-    ret = subprocess.call(['tests/utils/run.py', '-r', 'raetsch', '-d',
-                           'banana', '-e', 'tests/utils/MLPClassifier.json'])
+    ret = subprocess.call(['skdatasets/tests/utils/run.py', '-r', 'raetsch', '-d',
+                           'banana', '-e', 'skdatasets/tests/utils/MLPClassifier.json'])
     assert ret >= 0
 
 
 def test_multiclass_classification():
     """Tests multiclass classification experiment."""
-    ret = subprocess.call(['tests/utils/run.py', '-r', 'sklearn', '-d', 'iris',
-                           '-e', 'tests/utils/MLPClassifier.json'])
+    ret = subprocess.call(['skdatasets/tests/utils/run.py', '-r', 'sklearn', '-d', 'iris',
+                           '-e', 'skdatasets/tests/utils/MLPClassifier.json'])
     assert ret >= 0
-    ret = subprocess.call(['tests/utils/run.py', '-r', 'uci', '-d', 'wine',
-                           '-e', 'tests/utils/MLPClassifier.json'])
+    ret = subprocess.call(['skdatasets/tests/utils/run.py', '-r', 'uci', '-d', 'wine',
+                           '-e', 'skdatasets/tests/utils/MLPClassifier.json'])
     assert ret >= 0
-    ret = subprocess.call(['tests/utils/run.py', '-r', 'libsvm', '-c',
+    ret = subprocess.call(['skdatasets/tests/utils/run.py', '-r', 'libsvm', '-c',
                            'multiclass', '-d', 'shuttle', '-e',
-                           'tests/utils/MLPClassifier.json'])
+                           'skdatasets/tests/utils/MLPClassifier.json'])
     assert ret >= 0
-    ret = subprocess.call(['tests/utils/run.py', '-r', 'libsvm', '-c',
+    ret = subprocess.call(['skdatasets/tests/utils/run.py', '-r', 'libsvm', '-c',
                            'multiclass', '-d', 'usps', '-e',
-                           'tests/utils/MLPClassifier.json'])
+                           'skdatasets/tests/utils/MLPClassifier.json'])
     assert ret >= 0
 
 
 def test_regression():
     """Tests regression experiment."""
-    ret = subprocess.call(['tests/utils/run.py', '-r', 'libsvm', '-c',
+    ret = subprocess.call(['skdatasets/tests/utils/run.py', '-r', 'libsvm', '-c',
                            'regression', '-d', 'housing', '-e',
-                           'tests/utils/MLPRegressor.json'])
+                           'skdatasets/tests/utils/MLPRegressor.json'])
     assert ret >= 0
