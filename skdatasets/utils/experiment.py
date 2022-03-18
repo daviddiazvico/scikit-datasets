92b8e53daaa319e9e09c298785495d3451c9f6f4
vnmabus <vnmabus@gmail.com>
2022-03-18 01:13:39 +0100

    Typing experiment.

--- skdatasets/utils/experiment.py
+++ skdatasets/utils/experiment.py
@@ -2,19 +2,28 @@
 @author: David Diaz Vico
 @license: MIT
 """
+from __future__ import annotations
+
+import os
+from tempfile import NamedTemporaryFile, mkdtemp
+from time import process_time
+from typing import Callable, List, Mapping
+from warnings import warn
 
 import joblib
 import numpy as np
-import os
 from sacred import Experiment, Ingredient
-from sklearn.model_selection import cross_validate, PredefinedSplit
-from tempfile import mkdtemp, NamedTemporaryFile
-from time import process_time
-from warnings import warn
+from sklearn.base import BaseEstimator
+from sklearn.model_selection import PredefinedSplit, cross_validate
+from sklearn.utils import Bunch
 
 
-def experiment(dataset, estimator):
-    """Prepare a Scikit-learn experiment as a Sacred experiment.
+def experiment(
+    dataset: Callable[..., Bunch],
+    estimator: Callable[..., BaseEstimator],
+) -> Experiment:
+    """
+    Prepare a Scikit-learn experiment as a Sacred experiment.
 
     Prepare a Scikit-learn experiment indicating a dataset and an estimator and
     return it as a Sacred experiment.
@@ -22,9 +31,10 @@ def experiment(dataset, estimator):
     Parameters
     ----------
     dataset : function
-        Dataset fetch function. Might receive any argument. Must return a Bunch
-        with data, target (might be None), inner_cv (might be None) and outer_cv
-        (might be None).
+        Dataset fetch function. Might receive any argument. Must return a
+        :external:obj:`Bunch` with ``data``, ``target`` (might be ``None``),
+        ``inner_cv`` (might be ``None``) and ``outer_cv``
+        (might be ``None``).
     estimator : function
         Estimator initialization function. Might receive any keyword argument.
         Must return an initialized sklearn-compatible estimator.
@@ -35,107 +45,139 @@ def experiment(dataset, estimator):
         Sacred experiment, ready to be run.
 
     """
-
-    _dataset = Ingredient('dataset')
-    dataset = _dataset.capture(dataset)
-    _estimator = Ingredient('estimator')
-    estimator = _estimator.capture(estimator)
-    experiment = Experiment(ingredients=(_dataset, _estimator))
+    dataset_ingredient = Ingredient('dataset')
+    dataset = dataset_ingredient.capture(dataset)
+    estimator_ingredient = Ingredient('estimator')
+    estimator = estimator_ingredient.capture(estimator)
+    experiment = Experiment(
+        ingredients=(
+            dataset_ingredient,
+            estimator_ingredient,
+        ),
+    )
 
     @experiment.automain
-    def run():
+    def run() -> None:
         """Run the experiment."""
         data = dataset()
 
         # Metaparameter search
         X = data.data
         y = data.target
-        cv = None
-        explicit_cv_folds = False
-        if hasattr(data, 'inner_cv'):
-            cv = data.inner_cv
-            explicit_cv_folds = hasattr(data.inner_cv, '__iter__')
-            if explicit_cv_folds:
-                # Explicit CV folds
-                X = np.array([]).reshape((0, *data.inner_cv[0][0].shape[1:]))
-                y = np.array([]).reshape((0, *data.inner_cv[0][1].shape[1:]))
-                cv = []
-                for i, (X_, y_, X_test_, y_test_) in enumerate(data.inner_cv):
-                    X = np.concatenate((X, X_, X_test_))
-                    y = np.concatenate((y, y_, y_test_))
-                    cv = cv + [-1]*len(X_) + [i]*len(X_test_)
-                cv = PredefinedSplit(cv)
+        cv = getattr(data, 'inner_cv', None)
+
+        train_indices = getattr(data, 'train_indices', [])
+        validation_indices = getattr(data, 'validation_indices', [])
+        test_indices = getattr(data, 'test_indices', [])
+
+        X_train_val = (
+            X[train_indices + validation_indices]
+            if train_indices
+            else X
+        )
+        y_train_val = (
+            y[train_indices + validation_indices]
+            if train_indices
+            else y
+        )
+
+        X_test = X[test_indices]
+        y_test = y[test_indices]
+
         try:
             e = estimator(cv=cv)
-        except Exception as exception:
+        except TypeError as exception:
             warn(f'The estimator does not accept cv: {exception}')
             e = estimator()
-        if explicit_cv_folds:
-            e.fit(X, y=y)
+
+        if cv is not None:
+            e.fit(X_train_val, y_train_val)
             e.fit = e.best_estimator_.fit
 
         # Model assessment
-        if hasattr(data, 'data_test') and (data.data_test is not None):
+        if test_indices:
             # Test partition
-            e.fit(X, y=y)
+            e.fit(X_train_val, y_train_val)
             try:
                 with NamedTemporaryFile() as tmpfile:
                     joblib.dump(e, tmpfile.name)
-                    experiment.add_artifact(tmpfile.name,
-                                            name='estimator.joblib')
+                    experiment.add_artifact(
+                        tmpfile.name,
+                        name='estimator.joblib',
+                    )
             except Exception as exception:
                 warn(f'Artifact save failed: {exception}')
-            experiment.log_scalar('score_mean', e.score(data.data_test,
-                                  y=data.target_test))
+            experiment.log_scalar(
+                'score_mean',
+                e.score(X_test, y_test),
+            )
             experiment.log_scalar('score_std', 0.0)
             for output in ('transform', 'predict'):
                 if hasattr(e, output):
-                    with open(os.path.join(mkdtemp(), f'{output}.npy'),
-                              'wb+') as tmpfile:
-                        np.save(tmpfile, getattr(e, output)(data.data_test))
+                    with open(
+                        os.path.join(
+                            mkdtemp(),
+                            f'{output}.npy',
+                        ),
+                        'wb+',
+                    ) as tmpfile:
+                        np.save(tmpfile, getattr(e, output)(X_test))
                         experiment.add_artifact(tmpfile.name)
         elif hasattr(data, 'outer_cv'):
             # Outer CV
-            if hasattr(data.outer_cv, '__iter__'):
-                # Explicit CV folds
-                scores = {'test_score': list(), 'train_score': list(),
-                          'fit_time': list(), 'score_time': list(),
-                          'estimator': list()}
-                outputs = {'transform': list(), 'predict': list()}
-                for X, y, X_test, y_test in data.outer_cv:
-                    t0 = process_time()
-                    e.fit(X, y=y)
-                    t1 = process_time()
-                    test_score = e.score(X_test, y=y_test)
-                    t2 = process_time()
-                    scores['test_score'].append(test_score)
-                    scores['train_score'].append(e.score(X, y=y))
-                    scores['fit_time'].append(t1 - t0)
-                    scores['score_time'].append(t2 - t1)
-                    scores['estimator'].append(e)
-                    for output in ('transform', 'predict'):
-                        if hasattr(e, output):
-                            outputs[output].append([getattr(e, output)(X_test)])
+            # Explicit CV folds
+            scores: Mapping[str, List[float]] = {
+                'test_score': [],
+                'train_score': [],
+                'fit_time': [],
+                'score_time': [],
+                'estimator': [],
+            }
+            outputs: Mapping[str, List[np.typing.NDArray[float]]] = {
+                'transform': [],
+                'predict': [],
+            }
+            for X, y, X_test, y_test in data.outer_cv:
+                t0 = process_time()
+                e.fit(X, y=y)
+                t1 = process_time()
+                test_score = e.score(X_test, y=y_test)
+                t2 = process_time()
+                scores['test_score'].append(test_score)
+                scores['train_score'].append(e.score(X, y=y))
+                scores['fit_time'].append(t1 - t0)
+                scores['score_time'].append(t2 - t1)
+                scores['estimator'].append(e)
                 for output in ('transform', 'predict'):
-                    if outputs[output]:
-                        with open(os.path.join(mkdtemp(), f'{output}.npy'),
-                                  'wb+') as tmpfile:
-                            np.save(tmpfile, np.array(outputs[output]))
-                            experiment.add_artifact(tmpfile.name)
-            else:
-                # Automatic/indexed CV folds
-                scores = cross_validate(e, data.data, y=data.target,
-                                        cv=data.outer_cv,
-                                        return_train_score=True,
-                                        return_estimator=True)
+                    if hasattr(e, output):
+                        outputs[output].append(
+                            getattr(e, output)(X_test),
+                        )
+            for output in ('transform', 'predict'):
+                if outputs[output]:
+                    with open(
+                        os.path.join(
+                            mkdtemp(),
+                            f'{output}.npy',
+                        ),
+                        'wb+',
+                    ) as tmpfile:
+                        np.save(tmpfile, np.array(outputs[output]))
+                        experiment.add_artifact(tmpfile.name)
+
             try:
                 with NamedTemporaryFile() as tmpfile:
                     joblib.dump(e, tmpfile.name)
                     experiment.add_artifact(tmpfile.name, name='scores.joblib')
             except Exception as exception:
                 warn(f'Artifact save failed: {exception}')
-            experiment.log_scalar('score_mean',
-                                  np.nanmean(scores['test_score']))
-            experiment.log_scalar('score_std', np.nanstd(scores['test_score']))
+            experiment.log_scalar(
+                'score_mean',
+                np.nanmean(scores['test_score']),
+            )
+            experiment.log_scalar(
+                'score_std',
+                np.nanstd(scores['test_score']),
+            )
 
     return experiment
