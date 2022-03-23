f13e63a986ccc7409f439c9ac02b16ce1e5b76bd
Carlos Ramos Carreño <vnmabus@gmail.com>
2022-03-23 18:11:20 +0100

    Improve tables. (#54)

--- skdatasets/utils/scores.py
+++ skdatasets/utils/scores.py
@@ -20,6 +20,24 @@ if TYPE_CHECKING:
     else:
         from typing_extensions import Literal
 
+    CorrectionLike = Literal[
+        None,
+        'bonferroni',
+        'sidak',
+        'holm-sidak',
+        'holm',
+        'simes-hochberg',
+        'hommel',
+        'fdr_bh',
+        'fdr_by',
+        'fdr_tsbh',
+        'fdr_tsbky',
+    ]
+
+    MultitestLike = Literal['kruskal', 'friedmanchisquare']
+
+    TestLike = Literal['mannwhitneyu', 'wilcoxon']
+
 
 def scores_table(
     datasets: Sequence[str],
@@ -70,6 +88,7 @@ def scores_table(
         else rankdata(m, method=method)
         for m in scores
     ])
+
     table = pd.DataFrame(data=scores, index=datasets, columns=estimators)
     for i, d in enumerate(datasets):
         for j, e in enumerate(estimators):
@@ -77,28 +96,27 @@ def scores_table(
             if stds is not None:
                 table.loc[d, e] += f' ±{stds[i, j]:.{score_decimals}f}'
             table.loc[d, e] += f' ({ranks[i, j]:.{rank_decimals}f})'
+
     table.loc['rank mean'] = np.around(
         np.mean(ranks, axis=0),
         decimals=score_decimals,
     )
+
     return table
 
 
 def hypotheses_table(
     samples: np.typing.ArrayLike,
     models: Sequence[str],
+    *,
     alpha: float = 0.05,
-    multitest: Literal[None, 'kruskal', 'friedmanchisquare'] = None,
-    test: Literal['mannwhitneyu', 'wilcoxon'] = 'wilcoxon',
-    correction: Literal[
-        None, 'bonferroni', 'sidak', 'holm-sidak', 'holm',
-        'simes-hochberg', 'hommel', 'fdr_bh', 'fdr_by', 'fdr_tsbh',
-        'fdr_tsbky',
-    ] = None,
-    multitest_args: Mapping[str, Any] = dict(),
-    test_args: Mapping[str, Any] = dict(),
+    multitest: Optional[MultitestLike] = None,
+    test: TestLike = 'wilcoxon',
+    correction: CorrectionLike = None,
+    multitest_args: Optional[Mapping[str, Any]] = None,
+    test_args: Optional[Mapping[str, Any]] = None,
 ) -> Tuple[Optional[pd.DataFrame], Optional[pd.DataFrame]]:
-    """ 
+    """
     Hypotheses table.
 
     Prints a hypothesis table with a selected test and correction.
@@ -115,8 +133,8 @@ def hypotheses_table(
         Ranking multitest used.
     test: {'mannwhitneyu', 'wilcoxon'}, default='wilcoxon'
         Ranking test used.
-    correction: {'bonferroni', 'sidak', 'holm-sidak', 'holm',
-                 'simes-hochberg', 'hommel', 'fdr_bh', 'fdr_by', 'fdr_tsbh',
+    correction: {'bonferroni', 'sidak', 'holm-sidak', 'holm', \
+                 'simes-hochberg', 'hommel', 'fdr_bh', 'fdr_by', 'fdr_tsbh', \
                  'fdr_tsbky'}, default=None
         Method used to adjust the p-values.
     multitest_args: dict
@@ -132,35 +150,69 @@ def hypotheses_table(
     test_table: array-like
         Table of p-values and rejection/non-rejection for each test
         hypothesis.
+
     """
+    if multitest_args is None:
+        multitest_args = {}
+
+    if test_args is None:
+        test_args = {}
+
     samples = np.asanyarray(samples)
 
     versus = list(it.combinations(range(len(models)), 2))
-    comparisons = [models[vs[0]] + " vs " + models[vs[1]] for vs in versus]
-    multitests = {'kruskal': kruskal, 'friedmanchisquare': friedmanchisquare}
-    tests = {'mannwhitneyu': mannwhitneyu, 'wilcoxon': wilcoxon}
+    comparisons = [
+        f"{models[first]} vs {models[second]}"
+        for first, second in versus
+    ]
+
+    multitests = {
+        'kruskal': kruskal,
+        'friedmanchisquare': friedmanchisquare,
+    }
+    tests = {
+        'mannwhitneyu': mannwhitneyu,
+        'wilcoxon': wilcoxon,
+    }
+
     multitest_table = None
     if multitest is not None:
         multitest_table = pd.DataFrame(
             index=[multitest],
             columns=['p-value', 'Hypothesis'],
         )
-        statistic, pvalue = multitests[multitest](*samples, **multitest_args)
+        _, pvalue = multitests[multitest](
+            *samples.T,
+            **multitest_args,
+        )
         reject_str = 'Rejected' if pvalue <= alpha else 'Not rejected'
         multitest_table.loc[multitest] = ['{0:.2f}'.format(pvalue), reject_str]
+
+        # If the multitest does not detect a significative difference,
+        # the individual tests are not meaningful, so skip them.
         if pvalue > alpha:
             return multitest_table, None
-    pvalues = [tests[test](
-        samples[:, vs[0]],
-        samples[:, vs[1]],
-        **test_args,
-    )[1] for vs in versus]
+
+    pvalues = [
+        tests[test](
+            samples[:, first],
+            samples[:, second],
+            **test_args,
+        )[1] for first, second in versus
+    ]
+
     if correction is not None:
-        reject, pvalues, _, _ = multipletests(
+        reject_bool, pvalues, _, _ = multipletests(
             pvalues,
             alpha,
             method=correction,
         )
+        reject = [
+            'Rejected'
+            if r
+            else 'Not rejected'
+            for r in reject_bool
+        ]
     else:
         reject = [
             'Rejected'
@@ -168,10 +220,16 @@ def hypotheses_table(
             else 'Not rejected'
             for pvalue in pvalues
         ]
+
+    data = [
+        ('{0:.2f}'.format(p), r)
+        for p, r in zip(pvalues, reject)
+    ]
+
     test_table = pd.DataFrame(
+        data,
         index=comparisons,
         columns=['p-value', 'Hypothesis'],
     )
-    for i, d in enumerate(comparisons):
-        test_table.loc[d] = ['{0:.2f}'.format(pvalues[i]), reject[i]]
+
     return multitest_table, test_table
