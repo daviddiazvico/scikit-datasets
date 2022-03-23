e07da031b1b5b79b2217098f19a228f89ca4f8db
Carlos Ramos Carreño <vnmabus@gmail.com>
2022-03-23 18:17:38 +0100

    Typing experiment. (#49)
    * Typing experiment.
    
    * Fix outer_cv.
    
    * Split experiment function.
    
    * Allow `outer_cv` cv objects.
    
    * Fix estimator.
    
    * Fix tests.
    
    * Fix typo.

--- skdatasets/tests/utils/test_experiment.py
+++ skdatasets/tests/utils/test_experiment.py
@@ -2,16 +2,70 @@
 @author: David Diaz Vico
 @license: MIT
 """
+from __future__ import annotations
 
+import sys
+from typing import TYPE_CHECKING, Iterable, Tuple, Union
+
+import numpy as np
 from sacred.observers import FileStorageObserver
 from sklearn.datasets import load_boston
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.utils import Bunch
 
 from skdatasets.utils.experiment import experiment
 
+if TYPE_CHECKING:
+    IndicesType = np.typing.NDArray[int]
+    ExplicitSplitType = Iterable[
+        Tuple[
+            np.typing.NDArray[float],
+            np.typing.NDArray[Union[float, int]],
+            np.typing.NDArray[float],
+            np.typing.NDArray[Union[float, int]],
+        ],
+    ]
+    if sys.version_info >= (3, 8):
+        from typing import Protocol
+    else:
+        from typing_extensions import Protocol
+else:
+    IndicesType = np.ndarray
+    Protocol = object
 
-def _dataset(inner_cv=None, outer_cv=None):
+
+class CVSplitter(Protocol):
+
+    def split(
+        self,
+        X: np.typing.NDArray[float],
+        y: None = None,
+        groups: None = None,
+    ) -> Iterable[Tuple[IndicesType, IndicesType]]:
+        pass
+
+    def get_n_splits(
+        self,
+        X: np.typing.NDArray[float],
+        y: None = None,
+        groups: None = None,
+    ) -> int:
+        pass
+
+
+CVLike = Union[
+    CVSplitter,
+    Iterable[Tuple[IndicesType, IndicesType]],
+    int,
+    None,
+]
+
+
+def _dataset(
+    inner_cv: CVLike = None,
+    outer_cv: CVLike = None,
+) -> Bunch:
     data = load_boston()
     if outer_cv is None:
         X, X_test, y, y_test = train_test_split(data.data, data.target)
@@ -24,52 +78,95 @@ def _dataset(inner_cv=None, outer_cv=None):
         data.data_test = data.target_test = None
         data.outer_cv = outer_cv
     data.inner_cv = inner_cv
+
     return data
 
 
-def _estimator(cv):
-    return GridSearchCV(DecisionTreeRegressor(), {'max_depth': [2, 4]}, cv=cv)
+def _estimator(cv: CVLike) -> GridSearchCV:
+    return GridSearchCV(
+        DecisionTreeRegressor(),
+        {'max_depth': [2, 4]},
+        cv=cv,
+    )
 
 
-def _experiment(inner_cv, outer_cv):
+def _experiment(
+    inner_cv: CVLike,
+    outer_cv: CVLike | ExplicitSplitType,
+) -> None:
     e = experiment(_dataset, _estimator)
     e.observers.append(FileStorageObserver('.results'))
-    e.run(config_updates={'dataset': {'inner_cv': inner_cv,
-                                      'outer_cv': outer_cv}})
+    e.run(
+        config_updates={
+            'dataset': {
+                'inner_cv': inner_cv,
+                'outer_cv': outer_cv,
+            },
+        },
+    )
 
 
-def test_nested_cv():
+def test_nested_cv() -> None:
     """Tests nested CV experiment."""
     _experiment(3, 3)
 
 
-def test_inner_cv():
+def test_inner_cv() -> None:
     """Tests inner CV experiment."""
     _experiment(3, None)
 
 
-def test_explicit_inner_folds():
+def test_explicit_inner_folds() -> None:
     """Tests explicit inner folds experiment."""
     X, y = load_boston(return_X_y=True)
-    _experiment([[X[:10], y[:10], X[10:20], y[10:20]],
-                 [X[10:20], y[10:20], X[20:30], y[20:30]],
-                 [X[20:30], y[20:30], X[30:40], y[30:40]]], 3)
+    _experiment(
+        [
+            (np.arange(10), np.arange(10, 20)),
+            (np.arange(10, 20), np.arange(20, 30)),
+            (np.arange(20, 30), np.arange(30, 40)),
+        ],
+        3,
+    )
 
 
-def test_explicit_outer_folds():
+def test_explicit_outer_folds_indexes() -> None:
     """Tests explicit outer folds experiment."""
     X, y = load_boston(return_X_y=True)
-    _experiment(3, [[X[:10], y[:10], X[10:20], y[10:20]],
-                    [X[10:20], y[10:20], X[20:30], y[20:30]],
-                    [X[20:30], y[20:30], X[30:40], y[30:40]]])
+    _experiment(
+        3,
+        [
+            (np.arange(10), np.arange(10, 20)),
+            (np.arange(10, 20), np.arange(20, 30)),
+            (np.arange(20, 30), np.arange(30, 40)),
+        ],
+    )
 
 
-def test_explicit_nested_folds():
+def test_explicit_outer_folds() -> None:
+    """Tests explicit outer folds experiment."""
+    X, y = load_boston(return_X_y=True)
+    _experiment(
+        3,
+        [
+            (X[:10], y[:10], X[10:20], y[10:20]),
+            (X[10:20], y[10:20], X[20:30], y[20:30]),
+            (X[20:30], y[20:30], X[30:40], y[30:40]),
+        ],
+    )
+
+
+def test_explicit_nested_folds() -> None:
     """Tests explicit nested folds experiment."""
     X, y = load_boston(return_X_y=True)
-    _experiment([[X[:10], y[:10], X[10:20], y[10:20]],
-                 [X[10:20], y[10:20], X[20:30], y[20:30]],
-                 [X[20:30], y[20:30], X[30:40], y[30:40]]],
-                [[X[:10], y[:10], X[10:20], y[10:20]],
-                 [X[10:20], y[10:20], X[20:30], y[20:30]],
-                 [X[20:30], y[20:30], X[30:40], y[30:40]]])
+    _experiment(
+        [
+            (np.arange(3, 10), np.arange(3)),
+            (np.concatenate((np.arange(3), np.arange(7, 10))), np.arange(3, 7)),
+            (np.arange(7, 10), np.arange(7)),
+        ],
+        [
+            (np.arange(10), np.arange(10, 20)),
+            (np.arange(10, 20), np.arange(20, 30)),
+            (np.arange(20, 30), np.arange(30, 40)),
+        ],
+    )
